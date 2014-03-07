76efc8ca92d7e62f6bd07a29fe19cb9470fa3fe5
Pedro Vasconcelos <pedro.vasconcelos@stepvalue.com>
2014-03-07 15:52:23 +0000

    Update database.php

--- app/config/pedro-vasconcelos/database.php
+++ app/config/pedro-vasconcelos/database.php
@@ -7,7 +7,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'royalcanin-registo',
+			'database'  => 'database',
 			'username'  => 'root',
 			'password'  => '123456',
 			'charset'   => 'utf8',
